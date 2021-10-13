59f90b0d089cb8f0b1a01b030430222c1f963e86
Wesley Hamilton <hamilton@math.utah.edu>
2021-10-13 09:18:35 -0400

    Adds initial debugging for DtN code and network tools

--- DtN_tools.py
+++ DtN_tools.py
@@ -8,10 +8,10 @@ def schur_comp(M,idx_set):
     computes the schur complement/the pieces of the schur complement for a matrix M
     """
     comp_idx = [i for i in range(len(M)) if i not in idx_set]
-    A = M[np.idx_(idx_set,idx_set)]
-    B = M[np.idx_(idx_set,comp_idx)]
-    C = M[np.idx_(comp_idx,idx_set)]
-    D = M[np.idx_(comp_idx,comp_idx)]
+    A = M[np.ix_(idx_set,idx_set)]
+    B = M[np.ix_(idx_set,comp_idx)]
+    C = M[np.ix_(comp_idx,idx_set)]
+    D = M[np.ix_(comp_idx,comp_idx)]
 
 
     return A - B @ np.linalg.inv(D) @ C
@@ -21,8 +21,8 @@ def steklov_spec(M,bdy_idx):
     computes the steklov spectrum corresponding to a Laplacian M and boundary nodes bdy_idx
     """
 
-    eig_vals, eigvecs = np.linalg.eig(schur_comp(M,bdy_idx))
+    eigvals, eigvecs = np.linalg.eig(schur_comp(M,bdy_idx))
 
-    eig_val_sort = np.argsort(eigvals)
+    eigval_sort = np.argsort(eigvals)
 
-    return eig_vals[eig_val_sort], eigvecs.T[eig_val_sort]
+    return eigvals[eigval_sort], eigvecs.T[eigval_sort]
