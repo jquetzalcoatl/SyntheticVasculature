f7df9365ea4ae769915415969a7c65c7586d1ac6
Matt Phillips <mattphillipsphd@gmail.com>
2021-12-08 13:01:41 -0500

    Added 2d to 3d point conversion

--- simulations/add_vessels.py
+++ simulations/add_vessels.py
@@ -24,8 +24,10 @@ def main(cfg):
     blank2 = blank.copy()
     ht,wd,ch = blank.shape
 
-    coords = np.load( open(pj(cfg["vessels_dir"], "synet_coords.npy"), "rb") )
-    edges = np.load( open(pj(cfg["vessels_dir"], "synet_edges.npy"), "rb") )
+    coords = np.load( open(pj(cfg["vessels_dir"], "sim_retina_coords.npy"),
+        "rb") )
+    edges = np.load( open(pj(cfg["vessels_dir"], "sim_retina_edges.npy"),
+        "rb") )
     radii = np.load( open(pj(cfg["vessels_dir"],
         "sim_retina_radii-vein_radius-0.100.npy"), "rb") )
     for e_i,edge in enumerate(edges):
@@ -36,14 +38,12 @@ def main(cfg):
         p1 = (1+p1) * ht / 2
         p0 = [int(x) for x in p0]
         p1 = [int(x) for x in p1]
-#        e_i = min( e_i, len(radii)-1 )
+        e_i = min( e_i, len(radii)-1 )
 #        thickness = int( max(1, 10 * radii[e_i]) )
-#        thickness = int( max(1, 10 * (radii[edge[0]] + radii[edge[1]]) / 2 ) )
-        thickness = int( max(1, 10 * radii[edge[0]] ) )
+        thickness = int( max(1, 10 * (radii[edge[0]] + radii[edge[1]]) / 2) )
+#        thickness = int( max(1, 10 * radii[edge[0]] ) )
         cv2.line(blank2,  p0[:2], p1[:2], color=(127,32,96),
-                thickness=1)
-#        cv2.line(blank,  [p0[1], p1[1]], [p0[2], p1[2]], color=(127,32,96),
-#                thickness=1)
+                thickness=thickness)
 #        if e_i == 100:
 #            break
 
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--blank", type=str,
             default=pj(HOME,
-                "Datasets/Retina/DRIVE/training/blanks/22_blank.png"))
+                "Datasets/Retina/DRIVE/training/blanks/21_blank.png"))
     parser.add_argument("-v", "--vessels-dir", type=str,
             default=pj(HOME, "Output/retina/plots"))
     cfg = vars( parser.parse_args() )
